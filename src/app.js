dc5344c37fd7a1d213e1e030539fd907cb573780
Geoff Topley <geoff.topley@gmail.com>
2020-10-26 14:38:13 +0000

    Remove header config from app.js

--- src/app.js
+++ src/app.js
@@ -5,22 +5,22 @@ const app = express();
 const router = express.Router();
 require("dotenv").config();
 
-app.use("/.netlify/functions/app", router, function (req, res, next) {
+// app.use("/.netlify/functions/app", router, function (req, res, next) {
 
-  // header indicates whether the response can be shared with requesting code from the given origin.
-  const domain =
-    process.env.NODE_ENV == "development"
-      ? "http://localhost:3000"
-      : "https://run-tap.netlify.app/";
-  res.header("Access-Control-Allow-Origin", domain);
+//   // header indicates whether the response can be shared with requesting code from the given origin.
+//   const domain =
+//     process.env.NODE_ENV == "development"
+//       ? "http://localhost:3000"
+//       : "https://run-tap.netlify.app/";
+//   res.header("Access-Control-Allow-Origin", domain);
 
-  // set header values to resolve CORS issues
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+//   // set header values to resolve CORS issues
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 router.get("/sessionData", (req, res) => {
   const code = req.query.code;
@@ -34,6 +34,8 @@ router.get("/sessionData", (req, res) => {
     .then((response) => res.send(JSON.stringify(response.data)))
     .catch((error) => res.send(error));
 });
+
+app.use("/.netlify/functions/app", router);
 
 // allows lambda to run
 module.exports.handler = serverless(app);
