d3dc5191c878ef21fb9d7c7a416281b6c1e515d6
Geoff Topley <geoff.topley@gmail.com>
2020-10-26 22:58:28 +0000

    Add cors library and set up for local testing and production

--- src/app.js
+++ src/app.js
@@ -1,39 +1,40 @@
 const express = require("express");
+const cors = require("cors");
 const serverless = require("serverless-http");
 const axios = require("axios");
-const app = express();
-const router = express.Router();
 require("dotenv").config();
 
-// app.use("/.netlify/functions/app", router, function (req, res, next) {
-
-//   // header indicates whether the response can be shared with requesting code from the given origin.
-//   const domain =
-//     process.env.NODE_ENV == "development"
-//       ? "http://localhost:3000"
-//       : "https://run-tap.netlify.app/";
-//   res.header("Access-Control-Allow-Origin", domain);
-
-//   // set header values to resolve CORS issues
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
+const app = express();
+const router = express.Router();
 
 router.get("/sessionData", (req, res) => {
-  const code = req.query.code;
-  const client_id = process.env.CLIENT_ID;
-  const client_secret = process.env.CLIENT_SECRET;
-  
-  const url = `https://www.strava.com/api/v3/oauth/token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`;
+  if (process.env.ENVIRONMENT == "production") {
+    const code = req.query.code;
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
 
-  axios
-    .post(url)
-    .then((response) => res.send(JSON.stringify(response.data)))
-    .catch((error) => res.send(error));
+    const url = `https://www.strava.com/api/v3/oauth/token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`;
+
+    axios
+      .post(url)
+      .then((response) => res.send(JSON.stringify(response.data)))
+      .catch((error) => res.send(error));
+  } else {
+    res.json({ msg: "Testing Local Lambda - Success" });
+  }
 });
+
+const domain =
+  process.env.ENVIRONMENT == "production"
+    ? "https://run-tap.netlify.app/"
+    : "http://localhost:3000";
+
+app.use(
+  cors({
+    origin: domain,
+    methods: "GET",
+  })
+);
 
 app.use("/.netlify/functions/app", router);
 
